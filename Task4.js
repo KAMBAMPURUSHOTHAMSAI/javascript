b41470e32040c7154beec4f0edc8f4b73f87011f
Sai <kambampurushothamsai@gmail.com>
2026-04-17 11:00:20 +0530

    first commit

---
+++ Task4.js
@@ -0,0 +1,5 @@
+console.log("Welcome Team");
+console.log("2026");
+console.log("true");
+
+
---
+++ Task6.js
@@ -0,0 +1,6 @@
+let Fruits=["Apple","Banana","Mango","Orange","Grapes"];
+console.log(Fruits);
+console.log(Fruits[0]);
+console.log(Fruits[Fruits.length-1]);
+
+
